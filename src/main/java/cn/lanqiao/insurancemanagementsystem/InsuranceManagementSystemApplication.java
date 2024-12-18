a1a9c044e9767ade233006f20c9f45a3d04b9f77
wangxiaotao666 <1438132052@qq.com>
2024-12-18 19:33:14 +0800

    first commit

--- src/main/java/cn/lanqiao/insurancemanagementsystem/InsuranceManagementSystemApplication.java
+++ src/main/java/cn/lanqiao/insurancemanagementsystem/InsuranceManagementSystemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("cn.lanqiao.insurancemanagementsystem.mapper")
 public class InsuranceManagementSystemApplication {
-
+//11
     public static void main(String[] args) {
         SpringApplication.run(InsuranceManagementSystemApplication.class, args);
     }
