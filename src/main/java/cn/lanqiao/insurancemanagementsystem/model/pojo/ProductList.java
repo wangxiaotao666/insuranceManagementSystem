1a02427d695a68e2bbd0fcc1d5acea7f4722c2a2
wangxiaotao666 <1438132052@qq.com>
2024-12-24 10:32:23 +0800

    更新了保险列表TAdminController

---
+++ src/main/java/cn/lanqiao/insurancemanagementsystem/model/pojo/ProductList.java
@@ -0,0 +1,16 @@
+package cn.lanqiao.insurancemanagementsystem.model.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductList {
+  private Integer product_id;
+  private String product_name;
+  private String product_desc;
+  private String product_cost;
+  private String product_time;
+}
