f4d1a0943e9fb7463cef04514dbad2fd7169ac0c
wangxiaotao666 <1438132052@qq.com>
2024-12-24 20:54:32 +0800

    更新了实体类OrderList.java

---
+++ src/main/java/cn/lanqiao/insurancemanagementsystem/model/pojo/OrderList.java
@@ -0,0 +1,16 @@
+package cn.lanqiao.insurancemanagementsystem.model.pojo;
+
+
+public class OrderList {
+
+  private Integer orderId;
+  private String orderName;
+  private String orderCost;
+  private String orderTime;
+  private String orderUname;
+  private String orderUid;
+  private String orderState;
+  private String productId;
+
+
+}
