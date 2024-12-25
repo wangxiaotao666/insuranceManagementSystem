51b5f42fdacfb2c3cfe38179584304b18880f5f0
31554 <3155445272@qq.com>
2024-12-25 20:16:35 +0800

    xml配置文件更新

---
+++ src/main/java/cn/lanqiao/insurancemanagementsystem/service/impl/OrderListServiceImpl.java
@@ -0,0 +1,35 @@
+package cn.lanqiao.insurancemanagementsystem.service.impl;
+
+import cn.lanqiao.insurancemanagementsystem.mapper.OrderListMapper;
+import cn.lanqiao.insurancemanagementsystem.mapper.TAdminMapper;
+import cn.lanqiao.insurancemanagementsystem.model.pojo.OrderList;
+import cn.lanqiao.insurancemanagementsystem.service.OrderListService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author author
+ * @since 2024-12-23
+ */
+@Service
+public class OrderListServiceImpl  implements OrderListService {
+    @Autowired
+    private OrderListMapper OrderListMapper;
+    @Override
+    public List<OrderList> getorderlist() {
+        List<OrderList> orderLists = OrderListMapper.getorderlist();
+        if(orderLists!=null){
+            return orderLists;
+        }else {
+            return null;
+        }
+    }
+}
+
