1ec62736768b25290892f09d23ca6007d2f98c88
xuhong <3430275263@qq.com>
2024-12-31 11:07:42 +0800

    完善了用户的个人申请赔偿列表

--- src/main/java/cn/lanqiao/insurancemanagementsystem/controller/UserIndemnityController.java
+++ src/main/java/cn/lanqiao/insurancemanagementsystem/controller/UserIndemnityController.java
@@ -1,14 +1,17 @@
 package cn.lanqiao.insurancemanagementsystem.controller;
 
-import cn.lanqiao.insurancemanagementsystem.mapper.UserIndemnityMapper;
+
 import cn.lanqiao.insurancemanagementsystem.model.pojo.OrderList;
 import cn.lanqiao.insurancemanagementsystem.service.impl.UserIndemnityServiceImpl;
 import cn.lanqiao.insurancemanagementsystem.utils.ResponseUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/userIndemnity")
@@ -27,6 +30,25 @@ public class UserIndemnityController {
             }
         } catch (Exception e) {
             return new ResponseUtils(400,"数据查询异常");
+        }
+    }
+
+    @PostMapping("/applyPayment")
+    public ResponseUtils applyPayment(@RequestBody Map<String, String> params) {
+        try {
+            String orderId = params.get("order_id");
+            String productId = params.get("product_id");
+
+            // 调用服务层处理申请
+            boolean success = userIndemnityService.applyPayment(orderId, productId);
+
+            if (success) {
+                return new ResponseUtils(200, "申请提交成功");
+            } else {
+                return new ResponseUtils(500, "申请提交失败");
+            }
+        } catch (Exception e) {
+            return new ResponseUtils(400, "申请处理异常: " + e.getMessage());
         }
     }
 }
--- src/main/java/cn/lanqiao/insurancemanagementsystem/mapper/UserIndemnityMapper.java
+++ src/main/java/cn/lanqiao/insurancemanagementsystem/mapper/UserIndemnityMapper.java
@@ -1,6 +1,7 @@
 package cn.lanqiao.insurancemanagementsystem.mapper;
 import cn.lanqiao.insurancemanagementsystem.model.pojo.OrderList;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -8,4 +9,9 @@ public interface UserIndemnityMapper {
     //   查询个人申请赔偿列表
     @Select("select * from order_list ")
     List<OrderList> getIndemnityList();
+    // 更新订单状态为申请赔付
+
+    @Update("update order_list set order_state = '2' where order_id = #{orderId} and product_id = #{productId}")
+
+    int updateOrderState(String orderId, String productId);
 }
